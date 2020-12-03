6999d4998931310b574ffc8cedbc9af2e3ed304d
PH Neutre <pierrehoel.charny@gmail.com>
2020-12-03 11:33:59 +0100

    Maze grid setup

--- TP_Pacman/Assets/Script/Maze.cs
+++ TP_Pacman/Assets/Script/Maze.cs
@@ -8,11 +8,19 @@ public class Maze : MonoBehaviour {
     const int X_MIN = 0, X_MAX = 40, Y_MIN = 0, Y_MAX = 40;
     const float POS_OFFSET = 0.5f, MAP_SCALE = 0.32f;
 
+    public static Maze Instance = null;
+
     [SerializeField] GameObject prefabMarker = null;
     Dictionary<Vector2Int, Node> grid;
     Transform goDebug;
 
     private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+
         goDebug = transform.GetChild(transform.childCount - 1);
     }
 
@@ -63,18 +71,16 @@ public class Maze : MonoBehaviour {
         List<Node> uselessNodes = new List<Node>();
         foreach(Node n in grid.Values) {
             Node[] neighbors = n.Neighbors.ToArray();
-            if (neighbors.Length == 2) {
-                //if (neighbors[0] == ) {
-                    uselessNodes.Add(n);
-                    int index0 = neighbors[0].Neighbors.IndexOf(n);
-                    neighbors[0].Neighbors[index0] = neighbors[1];
-                    int index1 = neighbors[1].Neighbors.IndexOf(n);
-                    neighbors[1].Neighbors[index1] = neighbors[0];
-                //}
+            if(n.IsHallway) {
+                uselessNodes.Add(n);
+                int index0 = neighbors[0].Neighbors.IndexOf(n);
+                neighbors[0].Neighbors[index0] = neighbors[1];
+                int index1 = neighbors[1].Neighbors.IndexOf(n);
+                neighbors[1].Neighbors[index1] = neighbors[0];
             }
         }
         foreach(Node n in uselessNodes) {
-            grid.Remove(n.Coordinate);
+            grid[n.Coordinate] = null;
         }
     }
 
@@ -85,71 +91,25 @@ public class Maze : MonoBehaviour {
         }
     }
 
+    public NodeType GetCellType(Vector2Int coordinate) {
+        Node node;
+        if (grid.TryGetValue(coordinate, out node)) {
+            if (node.IsTunnel) { return NodeType.Tunnel; }
+            if (node.IsCrossroad) { return NodeType.Crossroad; }
+            if (node.IsHallway) { return NodeType.Hallway; }
+        }
+        return NodeType.None;
+    }
+
     static Vector3 GetWorldPositionFromGrid(Vector2Int gridPosition) {
         return new Vector3(gridPosition.x + POS_OFFSET, gridPosition.y + POS_OFFSET) * MAP_SCALE;
     }
+    
+    static Vector2Int GetGridCoordFromPosition(Vector3 worldPosition) {
+        return new Vector2Int((int)(worldPosition.x / MAP_SCALE), (int)(worldPosition.y / MAP_SCALE));
+    }
 
     /*
-    static Vector3Int GetGridPositionFromWorld(Vector3 worldPosition) {
-        return new Vector3Int(worldPosition.x, worldPosition.y) * MAP_SCALE;
-    }*/
-
-    /*
-     
-
-        public static void StartExample() {
-            int[][] gsl = new int[][] {
-                new int[] { 1, 2 },
-                new int[] { 0, 3, 4 },
-                new int[] { 0 },
-                new int[] { 1 },
-                new int[] { 1, 5, 6, 7 },
-                new int[] { 4 },
-                new int[] { 4 },
-                new int[] { 4 } 
-            };
-
-            Node[] nodes = GenerateNodes(gsl);
-    Console.WriteLine("Shortest path from Node" + nodes[0].Name + " to Node" + nodes[7].Name + " is " + GetPathLength(nodes[0], nodes[7]) + " unit(s) long.");
-        }
-
-static Node[] GetNodes(Node startNode) {
-    Queue<Node> queue = new Queue<Node>();
-    List<Node> result = new List<Node>();
-    queue.Enqueue(startNode);
-    startNode.Depth = 0;
-
-    while(queue.Count > 0) {
-        Node node = queue.Dequeue();
-        result.Add(node);
-        foreach(Node n in node.Neighbors) {
-            if(n.Depth < 0) {
-                queue.Enqueue(n);
-                n.Depth = node.Depth + 1;
-            }
-        }
-    }
-    return result.ToArray();
-}
-
-
-static int GetPathLength(Node startNode, Node endNode) {
-    Queue<Node> queue = new Queue<Node>();
-    queue.Enqueue(startNode);
-    startNode.Depth = 0;
-
-    while(queue.Count > 0) {
-        Node node = queue.Dequeue();
-        foreach(Node n in node.Neighbors) {
-            if(n.Depth < 0) {
-                queue.Enqueue(n);
-                n.Depth = node.Depth + 1;
-                if(n == endNode) { return n.Depth; }
-            }
-        }
-    }
-    return -1;
-}
 static List<Node> GetPath(Node startNode, Node endNode) {
     Queue<Node> queue = new Queue<Node>();
     queue.Enqueue(startNode);
@@ -179,19 +139,6 @@ static List<Node> GetParentPath(Node node, List<Node> path = null) {
     }
     path.Add(node);
     return GetParentPath(node.Parent, path);
-}
-
-static Node[] GenerateNodes(int[][] gslArray) {
-    Node[] nodes = new Node[gslArray.Length];
-    for(int i = 0; i < nodes.Length; i++) {
-        nodes[i] = new Node("" + i);
-    }
-    for(int i = 0; i < nodes.Length; i++) {
-        for(int j = 0; j < gslArray[i].Length; j++) {
-            nodes[i].Neighbors.Add(nodes[gslArray[i][j]]);
-        }
-    }
-    return nodes;
 }
 */
 }
--- TP_Pacman/Assets/Script/Node.cs
+++ TP_Pacman/Assets/Script/Node.cs
@@ -3,6 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Node {
+
+    public bool IsTunnel {
+        get {
+            return Neighbors.Count == 1;
+        }
+    }
+    public bool IsHallway {
+        get {
+            if (Neighbors.Count == 2) {
+                return (Neighbors[0].Coordinate.x == Neighbors[1].Coordinate.x) || (Neighbors[0].Coordinate.y == Neighbors[1].Coordinate.y);
+                }
+            return false;
+        }
+    }
+    public bool IsCorner {
+        get {
+            if(Neighbors.Count == 2) {
+                return !IsHallway;
+            }
+            return false;
+        }
+    }
+    public bool IsCrossroad {
+        get {
+            return Neighbors.Count > 2;
+        }
+    }
     public string Name { get; private set; }
     public Vector2Int Coordinate { get; private set; }
     public int Depth { get; set; }
@@ -16,4 +43,8 @@ public class Node {
         Neighbors = new List<Node>();
         Parent = null;
     }
+}
+
+public enum NodeType {
+    None, Tunnel, Hallway, Corner, Crossroad
 }
